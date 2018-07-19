2288d022a91640b40737a9e4af41908538f30dfe
itisha23 <itishagupta.2396@gmail.com>
2018-07-19 13:02:30 +0530

    Create postOrderTraversalusingOneStackAnsSet.cpp

---
+++ postOrderTraversalusingOneStackAnsSet.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::postorderTraversal(TreeNode* root) {
+    
+    TreeNode *curr;
+    vector<int>ans;
+    stack<TreeNode*>st;
+    set<TreeNode*>se;
+    curr=root;
+    while(1)
+    {
+      while(curr!=NULL)
+        {
+            st.push(curr);
+            curr=curr->left;
+        }
+        if(st.empty()!=1)
+        {
+            curr=st.top();
+            if(curr->right==NULL ||(curr->right!=NULL && se.find(curr->right)!=se.end()))
+            {
+                ans.push_back(curr->val);
+                st.pop();
+                se.insert(curr);
+                curr=NULL;
+            }
+            else
+            
+          //  if(curr->right!=NULL && se.find(curr->right))
+            curr=curr->right;
+        }
+        else
+        break;
+        
+    }
+    return ans;
+}
